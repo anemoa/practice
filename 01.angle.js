2c721a6b19bd11990bc5dc3102d9cec364ee515f
anemoa <anemoia.yves@gmail.com>
2024-06-15 23:57:56 +0900

    study: 삼각형 각도 판별

---
+++ 01.angle.js
@@ -0,0 +1,20 @@
+function solution(angle) {
+    var answer = 0;
+
+    if( 0 < angle && angle < 90 ){
+        return answer = 1;
+    }else if(angle === 90){
+        return answer = 2;
+    }else if(90 < angle && angle < 180){
+        return answer = 3;
+    }else if(angle === 180){
+        return answer = 4;
+    }
+    
+    return answer;
+}
+
+
+console.log(solution(70));
+console.log(solution(91));
+console.log(solution(180));
