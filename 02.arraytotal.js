7d7246a2de1907f758305badcaffba1966746850
anemoa <anemoia.yves@gmail.com>
2024-06-16 22:38:29 +0900

    study: 나머지 몫 구하기

--- 02.arraytotal.js
+++ 02.arraytotal.js
@@ -1,16 +1,28 @@
-  function solution2(numbers){
-    var answer = 0;
-    let numleng = numbers.length;
-    
-    for(i = 0; i < numleng; i++){
-      answer += numbers[i];
-    }
-    let total = answer / numleng;
-    answer = total.toFixed(3);
-
-    return console.log(answer);
+function solution2(numbers){
+  var answer = 0;
+  let numleng = numbers.length;
+  
+  for(i = 0; i < numleng; i++){
+    answer += numbers[i];
   }
+  let total = answer / numleng;
+  answer = total.toFixed(3);
+  return console.log(answer);
+}
 
 
-  solution2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-  solution2([89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]);
+solution2([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+solution2([89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]);
+
+
+// 초기값: 0 첫 번째 반복: 0(누적값) + 1(현재값) = 1 
+// 두 번째 반복: 1(누적값) + 2(현재값) = 3 
+// 세 번째 반복: 3(누적값) + 3(현재값) = 6 
+// 네 번째 반복: 6(누적값) + 4(현재값) = 10 
+// 최종 반환값: 10
+
+
+// function solution(numbers) {
+//   var answer = numbers.reduce((a,b) => a+b, 0) / numbers.length;
+//   return answer;
+// }
--- 03.sum2n.js
+++ 03.sum2n.js
@@ -4,6 +4,7 @@ function solution3(n){
   for(i = 0; i <= n; i++){
     if(i % 2 === 0){
       answer += i;
+      console.log(answer);
     }
   }
   return console.log(answer);
@@ -14,3 +15,27 @@ function solution3(n){
 //   return half*(half+1);
 // } 더 줄이면 이렇게도 된다고 한다.
 solution3(4);
+
+
+// 두 수의 합
+function solution(num1, num2) {
+  var answer = num1 + num2;
+  return answer;
+}
+
+
+// console.log(2, 3);
+// console.log(100, 2);
+
+// 나머지 몫 구하기
+
+function solution2(num1, num2){
+  let answer = 0;
+  answer = Math.floor(num1 / num2);
+
+  return console.log(answer);
+
+}
+
+solution2(10, 5);
+solution2(7, 2);
