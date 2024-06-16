ba220b0205ebfbe7c6a3cfe32ff782d7fd038ada
anemoa <anemoia.yves@gmail.com>
2024-06-16 18:02:10 +0900

    study: 짝수 합

--- 01.angle.js
+++ 01.angle.js
@@ -35,19 +35,3 @@ function solutionA(angle) {
 
 
 
-  function solution3(n){
-    var answer = 0;
-
-    for(i = 0; i <= n; i++){
-      if(i % 2 === 0){
-        answer += i;
-      }
-    }
-    return console.log(answer);
-  }
-
-// function solution4(n) {
-//   var half = Math.floor(n/2);
-//   return half*(half+1);
-// } 더 줄이면 이렇게도 된다고 한다.
-  solution3(4);
