61330c62d5b34e0dea19fafc6080f3f86938a524
anemoa <anemoia.yves@gmail.com>
2024-07-07 23:43:22 +0900

    study: 삼총사

--- 09.nNum.js
+++ 09.nNum.js
@@ -9,4 +9,13 @@ function nNum(n, numlist){
   return answer
 }
 
-console.log(nNum(3, [3,4,5,6,7,8]))
+console.log(nNum(3, [3,4,5,6,7,8]));
+
+
+function sam(num){
+  let answer = [];
+
+  return answer;
+}
+
+console.log(sam([3,3,0-3,-4]))
