78df6c2cf2297010391449dfdebe66c5bfbb3752
anemoa <anemoia.yves@gmail.com>
2024-07-08 20:13:42 +0900

    study:  socket.io 양방향 채팅 완성

--- 09.nNum.js
+++ 09.nNum.js
@@ -18,4 +18,20 @@ function sam(num){
   return answer;
 }
 
-console.log(sam([3,3,0-3,-4]))
+console.log(sam([3,3,0-3,-4]));
+
+
+function jungbok(arr){
+  let answer = [];
+
+  for(i = 0; i < arr.length; i++){
+    if(arr[i] !== arr[i+1]){
+      answer.push(arr[i]);
+    }
+  }
+  
+  return answer;
+
+}
+
+console.log(jungbok([1,1,1,3,3,5,99,99]));
