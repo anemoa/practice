8c0edd997473fa3e419ec2e3a63a901d5ee23017
anemoa <anemoia.yves@gmail.com>
2024-07-11 13:47:31 +0900

    study:  삼각형의 완성조건

--- 01.angle.js
+++ 01.angle.js
@@ -27,11 +27,29 @@ function solutionA(angle) {
   //   }).length;
   // }
 
-  console.log(solution(70));
-  console.log(solution(91));
-  console.log(solution(180));
+//   console.log(solution(70));
+//   console.log(solution(91));
+//   console.log(solution(180));
 
 
+// 삼각형의 완성조건
 
+// function tri(sides){
+//     sides = sides.sort((a,b) => a - b);
+//     if(sides[0] + sides[1] > sides[2]){
+//         return answer = 1;
+//     }else{
+//         return answer = 2;
+//     }
+// }
 
+function tri(sides){
+    sides = sides.sort((a, b) => a - b);
 
+    return sides[0] + sides[1] > sides[2] ? 1 : 2;
+
+}
+
+console.log(tri([199, 72, 222]));
+
+// arr.sort((a,b) => a - b); => sort 메소드를 사용할 때 오름차순으로 하려면 이렇게 정의하면 된다
